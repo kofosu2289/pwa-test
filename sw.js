30749b99ad09571eb774e9a4e9b93c12dde110b8
Kenneth Ofosu <kofosu89@gmail.com>
2019-12-16 15:54:16 -0500

    add dynamic cache to fetch about and contact page when offline

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
   '/',
   '/index.html',
@@ -9,40 +10,45 @@ const assets = [
   '/css/materialize.min.css',
   '/img/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
-// install service worker
+// install event
 self.addEventListener('install', event => {
-  // console.log('service worker has been installed');
+  //console.log('service worker installed');
   event.waitUntil(
-    caches.open(staticCacheName).then(cache => {
+    caches.open(staticCacheName).then((cache) => {
       console.log('caching shell assets');
-      cache.addAll(assets)
+      cache.addAll(assets);
     })
   );
 });
 
 // activate event
 self.addEventListener('activate', event => {
-  // console.log('service worker has been activated');
+  //console.log('service worker activated');
   event.waitUntil(
     caches.keys().then(keys => {
-      // console.log(keys);
-      Promise.all(keys
-        .filter(key => key !== staticCacheName)
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
         .map(key => caches.delete(key))
-      )
+      );
     })
   );
 });
 
-// fetch even
+// fetch event
 self.addEventListener('fetch', event => {
-  // console.log('fetch event', event);
+  //console.log('fetch event', event);
   event.respondWith(
     caches.match(event.request).then(cacheRes => {
-      return cacheRes || fetch(event.request);
+      return cacheRes || fetch(event.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(event.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
     })
   );
 });
