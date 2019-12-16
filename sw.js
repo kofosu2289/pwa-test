a165fc606a4e7a9a81a568f93235e4e521cd21c9
Kenneth Ofosu <kofosu89@gmail.com>
2019-12-16 14:48:56 -0500

    add fetch event listener to service worker

--- sw.js
+++ sw.js
@@ -7,3 +7,8 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   console.log('service worker has been activated');
 });
+
+// fetch event
+self.addEventListener('fetch', event => {
+  console.log('fetch event', event);
+});
