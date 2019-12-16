89b729b64bbd733b6312e517e66f5eab9ceecea5
Kenneth Ofosu <kofosu89@gmail.com>
2019-12-16 17:47:07 -0500

    enable online/offline add and delete

--- js/db.js
+++ js/db.js
@@ -18,7 +18,32 @@ db.collection('recipes').onSnapshot((snapshot) => {
       renderRecipe(change.doc.data(), change.doc.id);
     }
     if (change.type === 'removed') {
-      
+      removeRecipe(change.doc.id);
     }
   })
 });
+
+// add new recipe
+const form = document.querySelector('form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const recipe = {
+    title: form.title.value,
+    ingredients: form.ingredients.value
+  };
+  db.collection('recipes').add(recipe)
+    .catch(err => console.log(err));
+  
+  form.title.value = '';
+  form.ingredients.value = '';
+});
+
+// delete recipe
+const recipeContainer = document.querySelector('.recipes');
+recipeContainer.addEventListener('click', event => {
+  // console.log(event);
+  if (event.target.tagName === 'I') {
+    const id = event.target.getAttribute('data-id');
+    db.collection('recipes').doc(id).delete();
+  }
+})
