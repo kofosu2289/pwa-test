d0d25ecd552630024b19566dbc3a49aa8710e86e
Kenneth Ofosu <kofosu89@gmail.com>
2019-12-16 17:08:37 -0500

    create function to update ui from firestore

--- js/db.js
+++ js/db.js
@@ -4,7 +4,7 @@ db.collection('recipes').onSnapshot((snapshot) => {
   snapshot.docChanges().forEach(change => {
     // console.log(change, change.doc.data(), change.doc.id);
     if (change.type === 'added') {
-      
+      renderRecipe(change.doc.data(), change.doc.id);
     }
     if (change.type === 'removed') {
       
